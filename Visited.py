cd5bcc2619280bc32bcbca630af15067e7f770da
guido1992 <rathkealex@gmail.com>
2024-06-12 22:01:13 +0100

    Add files via upload

--- Visited.py
+++ Visited.py
@@ -5,11 +5,11 @@ import plotly.express as px
 import pandas as pd
 
 # List of countries you've visited
-visited_countries = ['United States of America', 'Ireland', 'United Kingdom', 'France',
+visited_countries = ['United States of America', 'Ireland', 'United Kingdom', 'France','Iceland',
                      'Spain', 'Netherlands', 'Belgium', 'Germany', 'Denmark', 'Sweden', 'Finland',
                      'Italy', 'Switzerland', 'Austria', 'Croatia', 'Poland', 'Greece', 'Ghana',
-                     'Namibia', 'Botswana', 'Zambia', 'Zimbabwe', 'South Africa', 'Israel', 'Turkey',
-                     'China']
+                     'Namibia', 'Botswana', 'Zambia', 'Zimbabwe', 'South Africa', 'Mauritius', 'Israel',
+                     'Turkey', 'China']
 
 # Path to the CSV file
 csv_path = r'Countries-Continents.csv'  # Update this path if necessary
@@ -27,11 +27,26 @@ df.columns = ['continent', 'country']
 df['visited'] = df['country'].isin(visited_countries)
 
 # Create the world map
-fig = px.choropleth(df, locations='country', locationmode='country names', color='visited',
-                    color_continuous_scale=[[0, 'lightgrey'], [1, 'green']])
-                    #title="Countries Visited")
+fig = px.choropleth(
+    df,
+    locations='country',
+    locationmode='country names',
+    color='visited',
+    color_continuous_scale=[[0, 'lightgrey'], [1, 'green']],
+    #title="Countries Visited"
+)
 
-st.plotly_chart(fig)
+# Increase the size of the map and remove the border
+fig.update_layout(
+    autosize=False,
+    width=2500,  # Adjust the width
+    height=350,  # Adjust the height
+    margin=dict(l=0, r=0, b=0, t=0),  # Remove the border
+    paper_bgcolor='rgba(0,0,0,0)',  # Set the background color to transparent
+    plot_bgcolor='rgba(0,0,0,0)'  # Set the plot background color to transparent
+)
+
+fig.update_geos(showcoastlines=False, showframe=False)
 
 # Calculate continent statistics
 continent_stats = df.groupby('continent').agg(
@@ -41,5 +56,14 @@ continent_stats = df.groupby('continent').agg(
 
 continent_stats['percentage_visited'] = (continent_stats['visited_countries'] / continent_stats['total_countries']) * 100
 
-st.subheader("Continent Statistics")
-st.dataframe(continent_stats)
+# Create a row of summary boxes
+#st.subheader("Continent Statistics")
+cols = st.columns(len(continent_stats))
+for col, (continent, total, visited, percentage) in zip(cols, continent_stats.values):
+    col.metric(continent, f"{visited}/{total}", f"{percentage:.0f}%")
+
+
+st.plotly_chart(fig)
+
+#st.subheader("Continent Statistics")
+#st.dataframe(continent_stats)
